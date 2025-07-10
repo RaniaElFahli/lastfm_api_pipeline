9a6336fe8d9770b6e514a20658129f0f71b8e0c7
Rania <rania.epro@gmail.com>
2025-07-10 19:02:46 +0200

    add topalbums topgenres and daily listening models

--- app/schemas.py
+++ app/schemas.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel
+from datetime import datetime 
 
 class TopArtists(BaseModel):
     artist_name:str
@@ -8,3 +9,16 @@ class TopTracks(BaseModel):
     track_name:str
     artist_name:str
     listen_count:int
+
+class TopAlbums(BaseModel):
+    album_name:str
+    artist_name:str
+    listen_count:int
+
+class TopGenres(BaseModel):
+    genre_name:str
+    listen_count:int
+
+class Dailylisteningduration(BaseModel):
+    date_time:datetime
+    duration_count:int
