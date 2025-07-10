29b7e0d0c9384c22f0c5b385a935114c86f9251d
Rania <rania.epro@gmail.com>
2025-07-10 19:46:12 +0200

    correct response model type + add session dependency

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
-from app.sessions import session
+from fastapi import FastAPI, Depends
+from app.sessions import engine
 from app.models import Artists, Tracks, Albums, ArtistGenre, RecentTracks, MusicGenre
 from sqlalchemy import func, desc
+from sqlalchemy.orm import Session
 from app.schemas import TopArtists, TopTracks, TopAlbums, TopGenres, Dailylisteningduration, GenreDiversity
 import math
+from typing import List
 
 app = FastAPI(
     title="My musical dashboard", 
@@ -11,16 +13,23 @@ app = FastAPI(
     version="1.0"
 )
 
+def get_db_session():
+    db = Session(engine)
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 async def read_root():
     return {"message": "Bienvenue sur l'API Music Dashboard"}
 
 @app.get("/top-artists", 
-    response_model=TopArtists,
+    response_model=List[TopArtists],
     summary="Top artists",
     description="Top recently streamed artists with desired limit")
-async def get_top_artists(limit: int = 5):
-    top_artists_query = session.query(
+async def get_top_artists(limit: int = 5, db: Session = Depends(get_db_session)):
+    top_artists_query = db.query(
         Artists.artist_name,
         func.count(RecentTracks.artist_id).label('artist_listen_count')
     ).join(Artists, Artists.artist_id == RecentTracks.artist_id
@@ -35,12 +44,12 @@ async def get_top_artists(limit: int = 5):
 
 
 @app.get("/top-tracks", 
-    response_model=TopTracks,
+    response_model=List[TopTracks],
     summary="Top tracks",
     description="Top recently streamed tracks with desired limit")
-async def get_top_tracks(limit: int=5):
+async def get_top_tracks(limit: int=5, db: Session = Depends(get_db_session)):
 
-    top_tracks_query = session.query(
+    top_tracks_query = db.query(
         Tracks.track_title, 
         Artists.artist_name,
         func.count(RecentTracks.track_id).label("tracks_listen_count")
@@ -57,11 +66,11 @@ async def get_top_tracks(limit: int=5):
     ]
 
 @app.get("/top-albums", 
-    response_model=TopAlbums,
+    response_model=List[TopAlbums],
     summary="Top streamed albums",
     description="List of top streamed albums with desired limit")
-async def get_top_albums(limit: int=5):
-    top_albums_query = session.query(
+async def get_top_albums(limit: int=5, db: Session = Depends(get_db_session)):
+    top_albums_query = db.query(
         Albums.album_title, 
         Artists.artist_name, 
         func.count(RecentTracks.album_id).label("albums_listen_count")
@@ -78,11 +87,11 @@ async def get_top_albums(limit: int=5):
     ]
 
 @app.get("/top-genres", 
-    response_model=TopGenres,
+    response_model=List[TopGenres],
     summary="Top genres streamed",
     description="Top musical genres based on artists recently streamed with desired limit")
-async def get_top_genres(limit: int=5):
-    top_genres_query = session.query(
+async def get_top_genres(limit: int=5, db: Session = Depends(get_db_session)):
+    top_genres_query = db.query(
         MusicGenre.genre_name,
         func.count(RecentTracks.artist_id).label("artist_listen_count")
     ).join(ArtistGenre, ArtistGenre.artist_id == RecentTracks.artist_id
@@ -98,11 +107,11 @@ async def get_top_genres(limit: int=5):
     ]
 
 @app.get("/daily-listens", 
-        response_model=Dailylisteningduration,
+        response_model=List[Dailylisteningduration],
     summary="Daily listening duration",
     description="Daily listening duration in minutes based on recent tracks")
-async def get_daily_duration():
-    daily_duration_query = session.query(
+async def get_daily_duration(db: Session = Depends(get_db_session)):
+    daily_duration_query = db.query(
         func.date(RecentTracks.date_time).label("date"), 
         func.sum(Tracks.duration / 60000).label("duration_count_minutes")
     ).join(Tracks, Tracks.track_id == RecentTracks.track_id
@@ -115,11 +124,11 @@ async def get_daily_duration():
         date_time=date_time, duration_count=duration_count)
         for date_time, duration_count in results
     ]
-@app.get("/genre-diversity", response_model=GenreDiversity,
+@app.get("/genre-diversity", response_model=List[GenreDiversity],
     summary="Musical genres diversity",
     description="Entropy indicator of musical genres diversity bases on artists recently streamed")
-async def get_genre_diversity():
-    genres_query = session.query(
+async def get_genre_diversity(db: Session = Depends(get_db_session)):
+    genres_query = db.query(
         MusicGenre.genre_name,
         func.count(RecentTracks.artist_id).label("artist_listen_count")
     ).join(ArtistGenre, ArtistGenre.artist_id == RecentTracks.artist_id
