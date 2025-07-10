335ad528518107f611d91575e2910fbb79563491
Rania <rania.epro@gmail.com>
2025-07-10 19:27:56 +0200

    add genrediversity model

--- app/schemas.py
+++ app/schemas.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from datetime import datetime 
+from typing import List, Dict
 
 class TopArtists(BaseModel):
     artist_name:str
@@ -22,3 +23,8 @@ class TopGenres(BaseModel):
 class Dailylisteningduration(BaseModel):
     date_time:datetime
     duration_count:float
+
+class GenreDiversity(BaseModel):
+    genre_diversity: float
+    total_listens: int
+    genres: List[Dict[str, int]]
