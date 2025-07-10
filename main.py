f1081e64ce8a2fe0fbd0d45fd4b10d2fa05e5cba
Rania <rania.epro@gmail.com>
2025-07-10 16:07:30 +0200

    add top tracks endpoint

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.sessions import session
 from app.models import Artists, Tracks, AlbumGenre, Albums, MusicGenre, ArtistGenre, RecentTracks
 from sqlalchemy import func, desc 
-from app.schemas import TopArtists
+from app.schemas import TopArtists, TopTracks
 
 app = FastAPI(
     title="My musical dashboard", 
@@ -29,3 +29,23 @@ async def get_top_artists(limit: int = 5):
         for artist_name, artist_listen_count in results
     ]
 
+
+@app.get("/top-tracks")
+async def get_top_tracks(limit: int=5)
+
+    top_tracks_query = session.query(
+        Tracks.track_title, 
+        Artists.artist_name,
+        func.count(RecentTracks.track_id).label("tracks_listen_count")
+    ).join(Tracks, Tracks.track_id == RecentTracks.track_id
+    ).join(Artists, Artists.artist_id == Tracks.artist_id
+    ).join(RecentTracks, RecentTracks.track_id == Tracks.track_id
+    ).group_by(Tracks.track_title, Artists.artist_name
+    ).order_by(desc("tracks_listen_count")).limit(limit)
+
+    results = top_tracks_query.all()
+
+    return [
+        TopTracks(track_name=track_name, artist_name=artist_name, listen_count=tracks_listen_count)
+        for track_name, artist_name, tracks_listen_count in results
+    ]
