0c2713c55d3c183c4c0ac991781d6022c94c7397
Rania <rania.epro@gmail.com>
2025-07-10 16:07:22 +0200

    add Toptracks model

--- app/schemas.py
+++ app/schemas.py
@@ -3,3 +3,8 @@ from pydantic import BaseModel
 class TopArtists(BaseModel):
     artist_name:str
     listen_count:int
+
+class TopTracks(BaseModel):
+    track_name:str
+    artist_name:str
+    listen_count:int
