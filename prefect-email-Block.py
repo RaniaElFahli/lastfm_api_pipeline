14c53f8ee7138384046979cfa6fc78d8beb1e290
Rania <rania.epro@gmail.com>
2025-07-19 14:05:38 +0200

    add smtp port and smtp type to email block

--- prefect-email-Block.py
+++ prefect-email-Block.py
@@ -7,7 +7,9 @@ load_dotenv()
 credentials = EmailServerCredentials(
     username=os.getenv("EMAIL"),
     password=os.getenv("EMAIL_PASSWORD"),
-    smtp_server="smtp.gmail.com"
+    smtp_server="smtp.gmail.com", 
+    smtp_port= 465, 
+    smtp_type="SSL"
 )
 
 credentials.save("email-credentials")
